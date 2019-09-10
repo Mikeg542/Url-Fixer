1da40327b13303fe20c1e72cb9dcd63f609aa017
Mikeg542 <mgweb1@mail.com>
2019-09-10 13:37:03 -0400

    dpeloy

---
+++ script.js
@@ -0,0 +1,26 @@
+function convert() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  var manipulate = copyText.value;
+  manipulate = manipulate.split("?")[0];
+
+  manipulate = manipulate.replace("https://", "https://bench-");
+
+  manipulate = manipulate.replace("/jobs/", "/admin/jobs/");
+
+  var dummy = document.createElement("textarea");
+  // to avoid breaking orgain page when copying more words
+  // cant copy when adding below this code
+  // dummy.style.display = 'none'
+  document.body.appendChild(dummy);
+  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+  dummy.value = manipulate;
+  dummy.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the converted URL");
+}
