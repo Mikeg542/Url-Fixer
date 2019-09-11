ba7336173188ac2926fb901598ea9c6e79ca9dec
Mikeg542 <mgweb1@mail.com>
2019-09-11 16:15:16 -0400

    update

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ function convert() {
   /* Alert the copied text */
   alert("Copied the converted URL");
 }
+
+function goto() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  var manipulate = copyText.value;
+  manipulate = manipulate.split("?")[0];
+
+  manipulate = manipulate.replace("https://", "https://bench-");
+
+  manipulate = manipulate.replace("/jobs/", "/admin/jobs/");
+
+  window.location = manipulate;
+}
